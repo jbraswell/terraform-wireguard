55e5bb72d426b26f54336257be19ef15a2db1c54
jbraswell <10187286+jbraswell@users.noreply.github.com>
2022-02-25 00:41:53 -0500

    add multi client to ali cloud (#40)

--- terraform/alicloud/variables.tf
+++ terraform/alicloud/variables.tf
@@ -8,6 +8,14 @@ variable "region" {
   description = "The wireguard server's desired region. Valid regions at https://www.alibabacloud.com/help/en/doc-detail/89155.htm."
 }
 
+variable "clients" {
+  type        = map(string)
+  description = "Map of client names to ip addresses. Must be in the 10.10.10/24 subnet, and 10.10.10.1 is reserved for the server."
+  default = {
+    "default" = "10.10.10.2",
+  }
+}
+
 variable "access_key" {
   type        = string
   description = "The access key id."
