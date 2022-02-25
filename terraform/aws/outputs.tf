55831a131fbdc3f3fd55e2a309d1220cee142ffd
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2022-02-24 23:32:07 -0500

    add qr codes (#35)

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -2,3 +2,10 @@ output "client_config" {
   value       = { for k, v in data.wireguard_config_document.client : k => v.conf }
   description = "Client configuration."
 }
+
+output "client_config_qr_codes" {
+  value = {
+  for k, v in data.wireguard_config_document.client : k => "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${urlencode(v.conf)}"
+  }
+  description = "Client configuration qr code links."
+}
