714a357c10492642252cdd28f6bd29e328a13315
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-02-25 13:20:22 -0500

    multi client serverspace

--- terraform/serverspace/variables.tf
+++ terraform/serverspace/variables.tf
@@ -14,6 +14,14 @@ variable "region" {
   description = "The wireguard server's desired region. Valid regions are Moscow (ds1), Amsterdam (am2), Almaty (kz), New Jersey (nj3)."
 }
 
+variable "clients" {
+  type        = map(string)
+  description = "Map of client names to ip addresses. Must be in the 10.10.10/24 subnet, and 10.10.10.1 is reserved for the server."
+  default = {
+    "default" = "10.10.10.2",
+  }
+}
+
 variable "api_key" {
   type        = string
   description = "Serverspace api key."
