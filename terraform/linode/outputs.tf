eef870a90d0408c8273bbdaad5bcb8097009ffd6
jbraswell <10187286+jbraswell@users.noreply.github.com>
2022-02-25 00:22:42 -0500

    add multi client config to linode (#39)

--- terraform/linode/outputs.tf
+++ terraform/linode/outputs.tf
@@ -1,4 +1,9 @@
 output "client_config" {
-  value       = data.wireguard_config_document.client.conf
+  value       = { for k, v in data.wireguard_config_document.client : k => v.conf }
   description = "Client configuration."
+}
+
+output "client_config_qr_codes" {
+  value       = { for k, v in data.wireguard_config_document.client : k => "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${urlencode(v.conf)}" }
+  description = "Client configuration qr code links."
 }
