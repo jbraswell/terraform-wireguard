eb40bd04916fe6c45533a20bc75f9183ac8621af
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2022-02-25 21:27:47 -0500

    multiple clients for generic (#47)

--- terraform/generic/variables.tf
+++ terraform/generic/variables.tf
@@ -4,6 +4,14 @@ variable "private_key_path" {
   default     = "~/.ssh/id_rsa"
 }
 
+variable "clients" {
+  type        = map(string)
+  description = "Map of client names to ip addresses. Must be in the 10.10.10/24 subnet, and 10.10.10.1 is reserved for the server."
+  default = {
+    "default" = "10.10.10.2"
+  }
+}
+
 variable "ssh_user" {
   type        = string
   description = "The ssh user for instance."
